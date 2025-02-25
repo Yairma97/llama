f392cc5e9c07e8d22c793d29df8e7f77400250f4
mayifan <m672832006@gmail.com>
2025-02-25 23:35:52 +0800

    feat:训练数据

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import pstats
 import sys
 import textwrap
 
-from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, StorageContext, Settings
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, StorageContext, Settings, Document
 from llama_index.core.ingestion import IngestionPipeline, IngestionCache, DocstoreStrategy
 from llama_index.core.node_parser import SentenceSplitter, SimpleFileNodeParser
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
@@ -20,6 +20,7 @@ from FlagEmbedding import BGEM3FlagModel
 from typing import List
 from llama_index.vector_stores.milvus.utils import BaseSparseEmbeddingFunction
 import nest_asyncio
+
 nest_asyncio.apply()
 
 OPENAI_API_KEY = 'sk-wNDMy7OZEz18JiPiu2GXwhnSn5FDBnhW5V8itYzDJvq5FVeY'
@@ -60,30 +61,49 @@ class ExampleEmbeddingFunction(BaseSparseEmbeddingFunction):
             result[int(k)] = raw_output[k]
         return result
 
+
 Settings.text_splitter = SentenceSplitter(chunk_size=512, chunk_overlap=100)
-Settings.embed_model = HuggingFaceEmbedding(model_name=os.getenv("EMBEDDING_MODEL"),device='cpu',embed_batch_size=2)
-Settings.llm = OpenAILike(
-    model="gpt-4o-mini",
-    api_base=OPENAI_API_BASE,
-    api_key=OPENAI_API_KEY,
-    is_chat_model=True,
-)
+Settings.embed_model = HuggingFaceEmbedding(model_name=os.getenv("EMBEDDING_MODEL"), device='cpu', embed_batch_size=2)
+
+
+def generate_doc(filename: str) -> List[Document]:
+    # 原始数据
+    parser = PandasExcelReader()
+    file_extractor = {".xlsx": parser}
+    documents = SimpleDirectoryReader(input_files=[filename],
+                                      file_extractor=file_extractor).load_data()
+    print(len(documents))
+    print(documents[:1])
+    return documents
+
 
 if __name__ == '__main__':
-    # 向量数据库
-    vector_store = MilvusVectorStore(
-        dim=1024,
-        uri="http://localhost:19530",
-        # overwrite=True,
-        enable_sparse=True,
-        sparse_embedding_function=ExampleEmbeddingFunction(),
-        hybrid_ranker="RRFRanker",
-        hybrid_ranker_params={"k": 60},
+    # 管道
+    pipeline = IngestionPipeline(
+        transformations=[
+            Settings.text_splitter,
+            Settings.embed_model,
+        ],
+        # 向量数据库
+        vector_store=MilvusVectorStore(
+            dim=1024,
+            uri="http://localhost:19530",
+            # overwrite=True,
+            enable_sparse=True,
+            sparse_embedding_function=ExampleEmbeddingFunction(),
+            hybrid_ranker="RRFRanker",
+            hybrid_ranker_params={"k": 60},
+        ),
+        # 文档缓存
+        cache=IngestionCache(
+            cache=RedisKVStore.from_host_and_port(host="localhost", port=6379),
+            collection="cache",
+        ),
+        # 文档存储
+        docstore=RedisDocumentStore.from_host_and_port(
+            "localhost", 6379, namespace="doc_store"
+        )
     )
-    index = VectorStoreIndex.from_vector_store(
-        vector_store, embed_model=Settings.embed_model
-    )
-    # index.storage_context.persist(persist_dir="./milvus")
-    query_engine = index.as_query_engine(vector_store_query_mode="hybrid")
-    response = query_engine.query("INPATIENT_ILLNESS_CODE的中文字段意思是什么?他在哪张表里？")
-    print(textwrap.fill(str(response), 100))
+    print('-----------')
+    asyncio.run(pipeline.arun(documents=generate_doc('./data/18 HDR-病案模型目录.xlsx'), show_progress=True))
+    print('============')
